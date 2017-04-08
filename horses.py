289d6c83366da2442af9b554167ace6beaf71200
chibiboss <chibibossr@gmail.com>
2017-04-07 20:17:09 -0500

    firts commit

---
+++ horses.py
@@ -0,0 +1,12 @@
+import random
+
+class Horse:
+    def __init__(self, name, count = 0):
+        self.name = name
+        self.count = count
+
+    def go_steps(self, step):
+        self.count += step
+
+    def get_name(self):
+        return self.name
